c36596e7d31b222ab9140798cbc72fc097aff9d3
David Xchel Morales Hurtado <davidxchelmh@gmail.com>
2025-04-07 11:42:48 -0600

    Added pytest for decrypt_finder

---
+++ test_tests.py
@@ -0,0 +1,91 @@
+#!/bin/env python
+"""
+Xchel's logic challenges testing module
+"""
+import pytest
+from tests import decrypt_finder
+
+decrypt_data = (
+(
+    """
+    HELLO
+    BYE
+    ASDDSD FVSCS AS
+    """,
+    (0, -1)
+    ),
+(
+    """
+    HELLO
+    BYE
+    IFMMP CZF CSAX
+    """,
+    (2, 26)
+    ),
+(
+    """
+    HELLO
+    BYE
+    THE
+    THIS
+    IFMMP CZF SGD SGHR
+    """,
+    (2, 1)
+    ),
+(
+    """
+    HELLO
+    BYE
+    THE
+    THIS
+    IFMMPCZFUIFUIJT
+    """,
+    (4, 26)
+    ),
+)
+
+matrix_data = (
+(
+    """
+    1 1 1 1
+    0 0 0 0
+    1 1 1 1
+    0 0 0 0
+    """,
+    "OK"
+    ),
+(
+    """
+    1 0 1 0
+    0 1 0 1
+    1 0 1 0
+    0 1 0 1
+    """,
+    "OK"
+    ),
+(
+    """
+    1 0 1 0
+    0 1 0 1
+    1 1 1 0
+    0 1 0 1
+    """,
+    "Change (2, 3)"
+    ),
+(
+    """
+    1 0 1 0
+    0 1 1 1
+    1 1 1 0
+    0 1 0 1
+    """,
+    "Corrupt"
+    ),
+)
+
+@pytest.mark.parametrize("string, expected", decrypt_data)
+def test_decrypt_finder(string, expected):
+    """
+    Function that tests decrypt_finder using previously expected results.
+    """
+    assert decrypt_finder(string) == expected
